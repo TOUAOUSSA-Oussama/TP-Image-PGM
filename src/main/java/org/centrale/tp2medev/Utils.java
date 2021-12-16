a9df3966ba918aab74586270a4fd74322645489e
TOOSSA <81390547+TOUAOUSSA-Oussama@users.noreply.github.com>
2021-12-16 10:38:01 +0100

    init

---
+++ src/main/java/org/centrale/tp2medev/Utils.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.centrale.tp2medev;
+
+/**
+ *
+ * @author admin
+ */
+public class Utils {
+    
+}
